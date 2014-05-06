d4883993242a7924e531a4d098e0e2c6e9523092
arxterra <sean.c@aosaimage.com>
2014-05-06 13:48:11 -0700

    Create pinouts_robot.h

---
+++ arxterra_Robot_v1/pinouts_robot.h
@@ -0,0 +1,15 @@
+// Sensor to Arduino Mega ADK Pin Mapping
+#define MOTOR1CURRENT_PIN     A0   // Analog Pins
+#define MOTOR2CURRENT_PIN     A1
+#define TEMPERATURE_PIN       A2
+#define CLEAN_BAT_PIN         A3
+#define DIRTY_BAT_PIN         A4
+                              // Digital Pins
+#define TRIGGER_PIN_LEFT  22  // Arduino pin tied to trigger pin on ping sensor.
+#define ECHO_PIN_LEFT     23  // Arduino pin tied to echo pin on ping sensor.
+#define TRIGGER_PIN_RIGHT 24  // Arduino pin tied to trigger pin on ping sensor.
+#define ECHO_PIN_RIGHT    25  // Arduino pin tied to echo pin on ping sensor.
+#define SERVO_PIN_PAN     44  // Arduino pin tied to pan servo pin.
+#define SERVO_PIN_TILT    46  // Arduino pin tied to pan servo pin.  
+
+
